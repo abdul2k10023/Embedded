85d6703c2a54861ad28fbd8155094ef0a7c1a86f
abdul <haseebarain15@gmail.com>
2022-05-21 02:05:47 +0200

    successfully clone project fom cubemx and created cmakelists from it

---
+++ nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/abdul/workspace/Embedded/nucleo_initialize/startup_stm32l073xx.s" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/startup_stm32l073xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32L073xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32L0xx_HAL_Driver/Inc"
+  "../Drivers/STM32L0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32L0xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_cortex.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_cortex.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_dma.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_dma.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_exti.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_exti.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_flash.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_flash.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_flash_ex.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_flash_ex.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_flash_ramfunc.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_flash_ramfunc.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_gpio.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_gpio.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_i2c.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_i2c.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_i2c_ex.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_i2c_ex.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_pwr.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_pwr.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_pwr_ex.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_pwr_ex.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_rcc.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_rcc.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_rcc_ex.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_rcc_ex.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_tim.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_tim.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_tim_ex.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_tim_ex.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_uart.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_uart.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_uart_ex.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_uart_ex.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Src/main.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Src/main.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Src/stm32l0xx_hal_msp.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Src/stm32l0xx_hal_msp.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Src/stm32l0xx_it.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Src/stm32l0xx_it.c.obj"
+  "/home/abdul/workspace/Embedded/nucleo_initialize/Src/system_stm32l0xx.c" "/home/abdul/workspace/Embedded/nucleo_initialize/build/CMakeFiles/nucleo_l073rz_initialize.out.dir/Src/system_stm32l0xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32L073xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32L0xx_HAL_Driver/Inc"
+  "../Drivers/STM32L0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32L0xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
